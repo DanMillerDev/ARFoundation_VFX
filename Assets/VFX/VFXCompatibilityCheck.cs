565cab4cdba495b61aedcf7402402c24e98bf5af
DanMillerDev <danmiller126407@gmail.com>
2019-10-26 19:49:42 -0700

    initial project commit

---
+++ Assets/VFX/VFXCompatibilityCheck.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class VFXCompatibilityCheck : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_NotSupportedUI;
+
+    void Awake()
+    {
+        // currently VFX graph and AR Foundation are only supported on iOS with Metal 
+        if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.Metal)
+        {
+            m_NotSupportedUI.SetActive(true);
+        }
+    }
+}
